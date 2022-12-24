834347da62d5f2fec8079f32e4f47d1ca3da41db
oskarkonitz <37907613+oskarkonitz@users.noreply.github.com>
2022-12-24 16:05:16 +0100

    Add files via upload

---
+++ Simple Calculator/Simple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Oskar Konitz on 24/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

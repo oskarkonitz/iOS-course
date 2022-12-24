3a61c48f70977647cf9f3669c79261320793dba3
Oskar Konitz <37907613+oskarkonitz@users.noreply.github.com>
2022-12-24 22:28:33 +0100

    Add files via upload

--- Simple Calculator/Simple Calculator/ViewController.swift
+++ Simple Calculator/Simple Calculator/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    func errorInt(){
+        resultLabel.text = "Nie wpisuj liter, tylko liczby."
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sumButtonClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }else{
+                errorInt()
+            }
+        }else{
+            errorInt()
+        }
+    }
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }else{
+                errorInt()
+            }
+        }else{
+            errorInt()
+        }
+    }
+    @IBAction func divideClicked(_ sender: Any) {
+        if secondText.text == "0"{
+            resultLabel.text = "Nie dziel przez 0"
+        }else if let firstNumber = Int(firstText.text!){
+                if let secondNumber = Int(secondText.text!){
+                    let result = firstNumber / secondNumber
+                    resultLabel.text = String(result)
+                }else{
+                    errorInt()
+                }
+            }else{
+                errorInt()
+            }
+    }
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }else{
+                errorInt()
+            }
+        }else{
+            errorInt()
+        }
+    }
+    
 }
 

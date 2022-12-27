f54f50eb96ec7b6e6abb9a071a03f26525240178
Oskar <oskar.konitz@gmail.com>
2022-12-27 22:38:10 +0100

    nowy projekt

---
+++ ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Oskar Konitz on 26/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
